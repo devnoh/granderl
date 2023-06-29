889f2793864993b035b0b50b56a65e5e44d0c9f1
Sehwan Noh <devnoh@gmail.com>
2023-06-28 17:54:47 -0700

    Update rdtsc.h

--- c_src/rdtsc.h
+++ c_src/rdtsc.h
@@ -7,13 +7,14 @@ inline void rdtsc(uint64_t *t, uint64_t *u)
 {
     asm volatile ("rdtsc" : "=a" (*t), "=d" (*u));
 }
-#elif defined(__aarch64__) && defined(__APPLE__)
+// #elif defined(__aarch64__) && defined(__APPLE__)
+#else
 #include <mach/mach_time.h>
 
 inline void rdtsc(uint64_t *t, uint64_t *u __attribute__((unused)))
 {
     *t = mach_absolute_time();
 }
-#else
-#error "Read your platform's perf counter here"
+// #else
+// #error "Read your platform's perf counter here"
 #endif
